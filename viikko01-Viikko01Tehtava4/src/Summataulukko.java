1fbcf2b3e9b6e2d0aea27c3431991aea40d8e7be
antjuntu <antjuntu@gmail.com>
2019-01-17 22:22:23 +0200

    1.4 ready, 1.3 started

--- viikko01-Viikko01Tehtava4/src/Summataulukko.java
+++ viikko01-Viikko01Tehtava4/src/Summataulukko.java
@@ -1,15 +1,23 @@
 
-import java.util.*;
 
 public class Summataulukko {
 
-    private int[] s;
+    private long[] summa;
     
     Summataulukko(long[] taulukko) {
+       summa = new long[taulukko.length];
+       summa[0] = taulukko[0];
        
+       for (int i = 1; i < taulukko.length; i++) {
+           summa[i] = summa[i - 1] + taulukko[i];
+       }
     }
 
     long summa(int l, int r) {
-        return 0;
+        if (l == 0) {
+            return summa[r];
+        } else {
+            return summa[r] - summa[l - 1];
+        }
     }
 }
--- viikko01-Viikko01Tehtava5/src/Piirileikki.java
+++ viikko01-Viikko01Tehtava5/src/Piirileikki.java
@@ -1,4 +1,25 @@
 public class Piirileikki {
+    
+    static int[] a;
+    
     public int viimeinen(int n) {
+        a = new int[n];
+        int counter = n;
+        for (int i = 0; i < n; i++) {
+            a[i] = i + 1;
+        }
+        
+        int k = 0;
+        boolean nextOut = true; // == false if skip next
+        while (counter > 0) {
+           
+            counter--;
+            if (counter == 0) {
+                return a[k];
+            }
+            a[k] = 0;
+        }
+        
+        return 0;
     }
 }
