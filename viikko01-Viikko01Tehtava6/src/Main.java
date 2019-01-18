cde9c96b1cfc7861b91aa640fae607073d270b12
antjuntu <antjuntu@gmail.com>
2019-01-18 17:47:15 +0200

    1.6 helper method tulosta added

--- viikko01-Viikko01Tehtava6/src/Main.java
+++ viikko01-Viikko01Tehtava6/src/Main.java
@@ -13,10 +13,28 @@ public class Main {
         File kansio = new File("test" + File.separator + "mockfiles");
         return rekursio(kansio, search);
     }
+    
+    public static void tulosta(File f) {
+        if (f.isFile()) {
+            System.out.println(f.getName());
+            return;
+        }
+        for (File file : f.listFiles()) {
+            if (file.isDirectory()) {
+                System.out.println("Dir: " + file.getName());
+            }
+            tulosta(file);
+        }
+    }
+    
 
     public static void main(String[] args) throws IOException {
-        System.out.println(laske("txt"));
-        System.out.println(laske("asd"));
-        System.out.println(laske("rekursio"));
+        System.out.println(laske("txt")); // 11
+        System.out.println(laske("asd")); // 0
+        System.out.println(laske("rekursio")); // 1
+        
+        File kansio = new File("test" + File.separator + "mockfiles");
+        tulosta(kansio);
+        
     }
 }
