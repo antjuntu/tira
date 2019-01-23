d2c5a9c870130c9b2781b8633edb0161412f275a
antjuntu <antjuntu@gmail.com>
2019-01-23 14:31:40 +0200

    2.5 ready

--- viikko02-Viikko02Tehtava5/src/Muutokset.java
+++ viikko02-Viikko02Tehtava5/src/Muutokset.java
@@ -1,4 +1,20 @@
 public class Muutokset {    
     public int laske(int[] taulukko) {
+        int changes = 0;
+     
+        int i = 0;
+        while (i < taulukko.length - 1) {
+            if (taulukko[i] == taulukko[i + 1]) {
+                changes++;
+                taulukko[i + 1] += taulukko[i];
+                if (i + 2 < taulukko.length) {
+                    taulukko[i + 1] += taulukko[i + 2];
+                }
+            }
+            i++;
+        }
+        
+        return changes;
     }    
+    
 }
