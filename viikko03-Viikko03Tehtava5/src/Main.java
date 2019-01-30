cb54fc10bc71114795c61a68a3b4f64b2a249572
antjuntu <antjuntu@gmail.com>
2019-01-30 15:55:41 +0200

    3.5 ready

--- viikko03-Viikko03Tehtava5/src/Main.java
+++ viikko03-Viikko03Tehtava5/src/Main.java
@@ -1,10 +1,24 @@
-
 import java.util.*;
 
 public class Main {
 
+    // Oletus taulukko.length > 1
     public static int montakoErilaista(String[] taulukko) {
-        return 0;
+        Arrays.sort(taulukko);
+        
+        int counter = 1; // montako erilaista
+   
+        int currentString = 0; // viimeisin erilainen 
+        
+        int i = 1;
+        while (i < taulukko.length) {
+            if (!taulukko[i].contentEquals(taulukko[currentString])) {
+                counter++;
+                currentString = i;
+            }
+            i++;
+        }
+        return counter;
     }
 
     public static void main(String[] args) {
@@ -12,5 +26,16 @@ public class Main {
         System.out.println(montakoErilaista(new String[]{"a", "b", "c", "d", "a"}));
         System.out.println(montakoErilaista(new String[]{"abba", "abbb", "bbba", "babb", "bbab"}));
         System.out.println(montakoErilaista(new String[]{"babb", "abbb", "bbba", "babb", "bbab"}));
+
     }
 }
+
+
+//        String a = "ntti";
+//        System.out.println(a.contentEquals("ntti"));
+        
+//        String[] strArr = new String[]{"abba", "abbb", "bbba", "babb", "bbab"};
+//        System.out.println(Arrays.toString(strArr));
+//        
+//        Arrays.sort(strArr);
+//        System.out.println(Arrays.toString(strArr));
