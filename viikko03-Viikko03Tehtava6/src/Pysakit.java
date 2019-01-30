985567a457a390a9cb2f61f7c1b845478831c9f9
antjuntu <antjuntu@gmail.com>
2019-01-30 16:29:54 +0200

    3.6 ready

--- viikko03-Viikko03Tehtava6/src/Pysakit.java
+++ viikko03-Viikko03Tehtava6/src/Pysakit.java
@@ -1,6 +1,29 @@
 import java.util.*;
 
 public class Pysakit {    
+    
     public int laske(int[] talot, int x) {
-    }    
+        if (talot.length == 0) {
+            return 0;
+        }
+        Arrays.sort(talot);
+        
+        // Asetetaan 1. pysäkki paikkaan talot[0] + x
+        int edellinenPysakki = talot[0] + x;
+        int lkm = 1; // pysäkkien määrä
+        
+        int i = 1; // seuraavan talon indeksi
+        while (i < talot.length) {
+            // Tarvitaanko uusi pysäkki?
+            if (talot[i] > edellinenPysakki + x) {
+                lkm++;
+                edellinenPysakki = talot[i] + x;
+            }
+            // Jos talon etäisyys pysäkistä on korkeintaan x,
+            // ohitetaan talo.
+            i++;
+        }
+        return lkm;
+    }   
+    
 }
