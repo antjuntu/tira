0426fd21856f621d6c6a369c186e988ec60d2bff
antjuntu <antjuntu@gmail.com>
2019-01-31 13:45:07 +0200

    3.7 linear solution ready

--- viikko03-Viikko03Tehtava7/src/Inversiot.java
+++ viikko03-Viikko03Tehtava7/src/Inversiot.java
@@ -1,37 +1,36 @@
 import java.util.*;
 
 public class Inversiot {   
-    // O(n^2) ratkaisu
+    // O(n)
     public int[] muodosta(int n, long k) {
+        int pienet = 1;
+        int suuret = n;
+        long inversiot = k;
         int[] tulos = new int[n];
-
-        for (int i = 0; i < n; i++) {
-            tulos[i] = i + 1;
-        }
-        if (k == 0) {
-            return tulos;
-        }
-        int lkm = 0;
-        while (true) {
-            int j = 0;
-            while (j < n - 1) {
-                if (tulos[j] < tulos[j + 1]) {
-                    int temp = tulos[j];
-                    tulos[j] = tulos[j + 1];
-                    tulos[j + 1] = temp;
-                    lkm++;
-                    if (lkm == k) {
-                        return tulos;
-                    }
-                }
-                j++;
-            }
-        }
         
+        int i = 0; // seuraavan sijoitettavan luvun paikka tulos-taulukossa
+        while (inversiot > 0) {
+            if (inversiot >= n - i - 1) {
+                tulos[i] = suuret;
+                suuret--;
+                inversiot -= n - i - 1;
+            } else {
+                tulos[i] = pienet;
+                pienet++;
+            }
+            i++;
+        }
+        // sijoitetaan loput luvut 
+        while(i < n) {
+            tulos[i] = pienet;
+            pienet++;
+            i++;
+        }
+        return tulos;
     }    
 }
 
-//// O(n^2) ratkaisu
+//// O(n^2)
 //    public int[] muodosta(int n, long k) {
 //        int[] tulos = new int[n];
 //
@@ -57,5 +56,4 @@ public class Inversiot {
 //                j++;
 //            }
 //        }
-//        
 //    }   
