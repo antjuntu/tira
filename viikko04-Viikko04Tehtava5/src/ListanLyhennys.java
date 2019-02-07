baf9eff06b53887d46c1aa5ed14ea4b64bfae45e
antjuntu <antjuntu@gmail.com>
2019-02-07 16:43:40 +0200

    4.5 ready

--- viikko04-Viikko04Tehtava5/src/ListanLyhennys.java
+++ viikko04-Viikko04Tehtava5/src/ListanLyhennys.java
@@ -1,6 +1,25 @@
 import java.util.*;
 
 public class ListanLyhennys {    
+    
+    static ArrayDeque<Integer> pino;
+    
     public int laske(int[] taulukko) {
+        pino = new ArrayDeque<>();
+        if (taulukko.length == 0) {
+            return 0;
+        }
+        pino.addLast(taulukko[0]);
+        int i = 1;
+        while (i < taulukko.length) {
+            Integer edellinen = pino.peekLast();
+            if (edellinen != null && edellinen == taulukko[i]) {
+                pino.pollLast();
+            } else {
+                pino.addLast(taulukko[i]);
+            }
+            i++;
+        }
+        return pino.size();
     }    
 }
