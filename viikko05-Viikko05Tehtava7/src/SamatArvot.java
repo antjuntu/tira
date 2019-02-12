68b04f07b327e080809155022ff282a49ebd2f05
antjuntu <antjuntu@gmail.com>
2019-02-12 13:29:22 +0200

    5.7 ok

--- viikko05-Viikko05Tehtava7/src/SamatArvot.java
+++ viikko05-Viikko05Tehtava7/src/SamatArvot.java
@@ -1,9 +1,18 @@
 import java.util.*;
 
-public class SamatArvot {    
+public class SamatArvot {   
+    
+    // kkC ->
+    // 107 * 31^2  + 107 * 31  + 67
+    // 107 * 31^2  + (107 - 1) * 31  + 67 + 31
+    // 107 * 31^2  + 106 * 31  + 98
+    // -> kjb
+    
     public String merkkijono1() {
+        return "kkC";
     }
 
     public String merkkijono2() {
+        return "kjb";
     }
 }
