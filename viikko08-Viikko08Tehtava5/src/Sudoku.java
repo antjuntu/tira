93e24c94f1a3a37bdc778def679be2289ce91f25
antjuntu <antjuntu@gmail.com>
2019-03-15 16:12:39 +0200

    8.5. ok

--- viikko08-Viikko08Tehtava5/src/Sudoku.java
+++ viikko08-Viikko08Tehtava5/src/Sudoku.java
@@ -4,12 +4,13 @@ import java.util.Scanner;
 public class Sudoku {
 
     static final int N = 9;
+    // varatut luvut talletetaan aputaulukoihin
     static boolean[][] vaaka;
     static boolean[][] pysty;
     // 3x3 alueet, jotka sisältävät luvut 1,...,9
     static boolean[][] alue;
     
-    // aluksi vapaat paikat
+    // Alussa tyhjät kohdat
     static int[] vapaaY;
     static int[] vapaaX;
     static int vapaidenLkm;
@@ -19,8 +20,8 @@ public class Sudoku {
         pysty = new boolean[N][N+1];
         alue = new boolean[N][N+1];
         
-        vapaaY = new int[N * N];
-        vapaaX = new int[N * N];
+        vapaaY = new int[N * N + 1];
+        vapaaX = new int[N * N + 1];
         
         int counter = 0;
         for (int i = 0; i < N; i++) {
@@ -37,16 +38,8 @@ public class Sudoku {
                 }
             }
         }
-        
         vapaidenLkm = counter;
         
-        
-        System.out.println("vaaka");
-        tulosta(vaaka);
-        System.out.println("pysty");
-        tulosta(pysty);
-        System.out.println("alue");
-        tulosta(alue);
         haku(sudoku, 0);
       
     }
@@ -66,7 +59,6 @@ public class Sudoku {
                 if (haku(sudoku, k + 1)){
                     return true;
                 }
-                //sudoku[y][x] = 0;
                 vaaka[y][i] = false;
                 pysty[x][i] = false;
                 alue[alueIndeksi(y, x)][i] = false;
@@ -75,44 +67,7 @@ public class Sudoku {
         return false;
     }
     
-//    static void haku(int[][] sudoku, int rivi, int sarake) {
-//        if (rivi == N) {
-//            // ratkaisu löytyi
-//            return;
-//        } 
-//        if (sarake == N) {
-//            haku(sudoku, rivi + 1, 0); 
-//        } else if(sudoku[rivi][sarake] != 0){ 
-//            haku(sudoku, rivi, sarake + 1);
-//        } else{
-//            for (int i = 1; i <= N; i++) {
-//                if (!vaaka[rivi][i] && !pysty[sarake][i] && !alue[alueIndeksi(rivi, sarake)][i]) {
-//                    vaaka[rivi][i] = true;
-//                    pysty[sarake][i] = true;
-//                    alue[alueIndeksi(rivi, sarake)][i] = true;
-//                    sudoku[rivi][sarake] = i;
-//                    haku(sudoku, rivi, sarake + 1);
-//                    vaaka[rivi][i] = false;
-//                    pysty[sarake][i] = false;
-//                    alue[alueIndeksi(rivi, sarake)][i] = false;
-//                }
-//            }
-//        }
-//    }
     
-    
-    static void tulosta(boolean [][] a) {
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N+1; j++) {
-                //System.out.print(a[i][j] + " ");
-                if (a[i][j]) {
-                    System.out.print(j + " ");
-                }
-            }
-            System.out.println();
-        }
-        System.out.println("-------------------");
-    }
     static int alueIndeksi(int rivi, int sarake) {
         return (rivi / 3) * 3 + sarake / 3;
     }
@@ -129,13 +84,6 @@ public class Sudoku {
         "5???62??8"};
 
     public static void main(String[] args) {
-        // Testataan alueIndeksi()
-//        for (int rivi = 0; rivi < 9; rivi++) {
-//            for (int sarake = 0; sarake < 9; sarake++) {
-//                System.out.println("rivi: " + rivi + ", sarake: " + sarake + " Alue: " + alueIndeksi(rivi, sarake));
-//            }
-//            System.out.println("----------------------");
-//        }
         int[][] sudoku = new int[9][9];
 
         sudoku = toIntArray(esim1);
@@ -161,5 +109,18 @@ public class Sudoku {
             }
         }
         return s;
+    }
+    
+    static void tulosta(boolean [][] a) {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N+1; j++) {
+                //System.out.print(a[i][j] + " ");
+                if (a[i][j]) {
+                    System.out.print(j + " ");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println("-------------------");
     }
 }
