d353ddcfd29e0991f875ea55b1205dd442e60977
antjuntu <antjuntu@gmail.com>
2019-04-05 13:34:39 +0300

    11.6 aloitettu

--- viikko11-Viikko11Tehtava6/src/Kolikot.java
+++ viikko11-Viikko11Tehtava6/src/Kolikot.java
@@ -2,14 +2,99 @@ import java.util.*;
 
 public class Kolikot {
     
+    static final int N = 500;
+    static final int VALKOINEN = 0;
+    static final int HARMAA = 1;
+    static final int MUSTA = 2;
+    
+    boolean[] huone;
+    int vari[];
+    boolean[] kasitelty;
+    int[] kolikoitaHuoneissa;
+    
+    ArrayList<Integer>[] vieruslista;
+    ArrayList<Integer>[] vieruslistaTranspoosi;
+    
+    // Kosarajun algoritmiin
+    ArrayDeque<Integer> lista;
+    
+    // Komponenttiverkko
+    int nC = 0;
+    int[] komponentti;
+    int[] kolikoitaKomponenteissa;
+    
+    ArrayList<Integer> vieruslistaC;
+    
+    public Kolikot() {
+        huone = new boolean[N + 1];
+        vari = new int[N + 1];
+        kasitelty = new boolean[N + 1];
+        kolikoitaHuoneissa = new int[N + 1];
+        
+        vieruslista = new ArrayList[N + 1];
+        vieruslistaTranspoosi = new ArrayList[N + 1];
+        for (int i = 1; i <= N; i++) {
+            vieruslista[i] = new ArrayList<>();
+            vieruslistaTranspoosi[i] = new ArrayList<>();
+        }
+        
+        lista = new ArrayDeque<>();
+    }
+    
     public void lisaaYhteys(int alku, int loppu) {
+        huone[alku] = true;
+        huone[loppu] = true;
+        vieruslista[alku].add(loppu);
+        vieruslistaTranspoosi[loppu].add(alku);
     }
     
     public void lisaaKolikko(int huone) {
+        kolikoitaHuoneissa[huone]++;
     }
     
     
     public int parasTulos(int alku, int loppu) {
+        
+        kosaraju();
+        
+        return 0;
     }
+    
+    void kosaraju() {
+        for (int i = 1; i <= N; i++) {
+            if (huone[i] && vari[i] == VALKOINEN) {
+                syvyyshaku1(i);
+            }
+        }
+        System.out.println("Lista:");
+        for (Integer i : lista) {
+            if (huone[i]) {
+                System.out.print(i + ", ");
+            }
+        }
+        System.out.println();
+    }
+    
+    void syvyyshaku1(int u) {
+        vari[u] = HARMAA;
+        for (Integer v : vieruslista[u]) {
+            if (!huone[v]) {
+                continue;
+            }
+//            if (vari[v] == HARMAA) {
+//                // sykli löytyi
+//            }
+            if (vari[v] == VALKOINEN) {
+                syvyyshaku1(v);
+            }
+        }
+        vari[u] = MUSTA;
+        lista.addFirst(u);
+    }
+    
+    void syvyyshaku2(int u) {
+        
+    }
+    
     
 }
--- viikko11-Viikko11Tehtava6/src/Main.java
+++ viikko11-Viikko11Tehtava6/src/Main.java
@@ -2,15 +2,15 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Kolikot k = new Kolikot();
-        k.lisaaYhteys(1,2);
-        k.lisaaYhteys(2,4);
-        k.lisaaYhteys(1,3);
-        k.lisaaYhteys(3,4);
-        k.lisaaKolikko(2);
-        k.lisaaKolikko(3);
-        k.lisaaKolikko(3);
-        System.out.println(k.parasTulos(1,4)); // 2
+//        Kolikot k = new Kolikot();
+//        k.lisaaYhteys(1,2);
+//        k.lisaaYhteys(2,4);
+//        k.lisaaYhteys(1,3);
+//        k.lisaaYhteys(3,4);
+//        k.lisaaKolikko(2);
+//        k.lisaaKolikko(3);
+//        k.lisaaKolikko(3);
+//        System.out.println(k.parasTulos(1,4)); // 2
         
         // oma
         Kolikot k2 = new Kolikot();
@@ -36,5 +36,6 @@ public class Main {
         k2.lisaaYhteys(15,16);
         k2.lisaaYhteys(16,13);
         k2.lisaaYhteys(14,5);
+        System.out.println(k2.parasTulos(1,16));
     }
 }
