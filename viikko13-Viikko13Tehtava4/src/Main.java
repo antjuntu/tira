e0930b0ef5b2e7a887965af654e1f5ab770a10b3
antjuntu <antjuntu@gmail.com>
2019-04-16 14:47:18 +0300

    13.4 ready

--- viikko13-Viikko13Tehtava4/src/Main.java
+++ viikko13-Viikko13Tehtava4/src/Main.java
@@ -2,15 +2,27 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+//        Verkkoyhteys v = new Verkkoyhteys();
+//        v.lisaaYhteys(1,2,2);
+//        v.lisaaYhteys(1,4,6);
+//        v.lisaaYhteys(2,3,3);
+//        v.lisaaYhteys(2,4,7);
+//        v.lisaaYhteys(3,4,2);
+//        v.lisaaYhteys(3,5,8);
+//        v.lisaaYhteys(4,3,4);
+//        v.lisaaYhteys(4,5,1);
+//        System.out.println(v.laskeNopeus(1,5)); // 7
+
         Verkkoyhteys v = new Verkkoyhteys();
-        v.lisaaYhteys(1,2,2);
-        v.lisaaYhteys(1,4,6);
-        v.lisaaYhteys(2,3,3);
-        v.lisaaYhteys(2,4,7);
-        v.lisaaYhteys(3,4,2);
-        v.lisaaYhteys(3,5,8);
-        v.lisaaYhteys(4,3,4);
-        v.lisaaYhteys(4,5,1);
-        System.out.println(v.laskeNopeus(1,5)); // 7
+        v.lisaaYhteys(1,2,1);
+        v.lisaaYhteys(2,3,1);
+        v.lisaaYhteys(3,6,1);
+        v.lisaaYhteys(1,5,1);
+        v.lisaaYhteys(5,3,1);
+        v.lisaaYhteys(2,4,1);
+        v.lisaaYhteys(4,6,1);
+        //int oikea = 2;
+        int tulos = v.laskeNopeus(1,6);
+        System.out.println(tulos);
     }
 }
--- viikko13-Viikko13Tehtava4/src/Verkkoyhteys.java
+++ viikko13-Viikko13Tehtava4/src/Verkkoyhteys.java
@@ -7,7 +7,6 @@ public class Verkkoyhteys {
     static final int MAX_NOPEUS = 100;
     
     //int maxVirtaus;
-    //ArrayList<Kaari>[] vieruslista;
     int[][] d;
     
     boolean[] vierailtu;
@@ -15,22 +14,27 @@ public class Verkkoyhteys {
     int[] edeltaja;
     
     public void lisaaYhteys(int alku, int loppu, int nopeus) {
-        //vieruslista[alku].add(new Kaari(alku, loppu, nopeus));
-        d[alku][loppu] = nopeus;
+
+        d[alku][loppu] += nopeus;
     }
     
     public int laskeNopeus(int alku, int loppu) {
         int maxVirtaus = 0;
-        
+//        for (int i = 1; i <= 5; i++) {
+//            for (int j = 1; j <= 5; j++) {
+//                System.out.print(d[i][j] + " ");
+//            }
+//            System.out.println();
+//        }
         boolean uusiPolkuLoytyi = false;
-        jono = new ArrayDeque<>();
+   
         do {
-            
+            uusiPolkuLoytyi = false;
             for (int i = 1; i <= V; i++) {
                 vierailtu[i] = false;
                 edeltaja[i] = 0;
             }
-            //jono = new ArrayDeque<>();
+            jono = new ArrayDeque<>();
             vierailtu[alku] = true;
             jono.addLast(alku);
             while(!jono.isEmpty()) {
@@ -48,26 +52,48 @@ public class Verkkoyhteys {
                 }
             }
             if (uusiPolkuLoytyi) {
-                jono = new ArrayDeque<>();
-                jono.addFirst(loppu);
+                ArrayDeque<Integer> jono1 = new ArrayDeque<>();
+                ArrayDeque<Integer> jono2 = new ArrayDeque<>();
+                jono1.addFirst(loppu);
+                jono2.addFirst(loppu);
                 int s = loppu;
                 while (s != alku) {
                     s = edeltaja[s];
-                    jono.addFirst(s);
+                    jono1.addFirst(s);
+                    jono2.addFirst(s);
                 }
-                System.out.println(jono);
+                
                 int minPituus = MAX_NOPEUS + 1;
-                s = jono.removeFirst();
-                while (!jono.isEmpty()) {
-                    int x = jono.removeFirst();
+                System.out.println(jono1);
+                s = jono1.removeFirst();
+                while (!jono1.isEmpty()) {
+                    int x = jono1.removeFirst();
                     if (d[s][x] < minPituus) {
                         minPituus = d[s][x];
                     }
                     s = x;
                 }
+                
                 System.out.println("minPituus: " + minPituus);
+                maxVirtaus += minPituus;
+                ///////////////////////////////////
+                //System.out.println(jono2);
+                s = jono2.removeFirst();
+                while (!jono2.isEmpty()) {
+                    int x = jono2.removeFirst();
+                    System.out.println("s: " + s + ", x: " + x);
+                    d[s][x] = d[s][x] - minPituus;
+                    d[x][s] = minPituus;
+                    s = x;
+                }
+//                for (int i = 1; i <= 5; i++) {
+//                    for (int j = 1; j <= 5; j++) {
+//                        System.out.print(d[i][j] + " ");
+//                    }
+//                    System.out.println();
+//                }
             }
-        } while (false);
+        } while (uusiPolkuLoytyi);
         
         return maxVirtaus;
     }
